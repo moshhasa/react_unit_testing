a3d451c56cd177be17d9658e6484b6e89f93eaa7
Ernest <ernestduodu@hotmail.co.uk>
2021-09-23 13:11:18 +0100

    Added mocking of http request

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -2,11 +2,25 @@ import { render, screen } from "@testing-library/react";
 import Async from "./Async ";
 
 describe("Async testing", () => {
+  test("Renders post if request is successful without mock (do not do this... it is bad)", async () => {
+    render(<Async />);
 
-    test("Renders post if request is successful", async () => {
-        render(<Async/>);
+    const listItemElements = await screen.findAllByRole(
+      "listitem",
+      {},
+      { timeout: 3000 }
+    );
+    expect(listItemElements).not.toHaveLength(0);
+  });
 
-        const listItemElements = await screen.findAllByRole("listitem", {}, {timeout: 3000});
-        expect(listItemElements).not.toHaveLength(0);
-    })
-})
+  test("Renders post if request is successful using mock", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", title: "First post" }],
+    });
+    render(<Async />);
+
+    const listItemElements = await screen.findAllByRole("listitem");
+    expect(listItemElements).not.toHaveLength(0);
+  });
+});
